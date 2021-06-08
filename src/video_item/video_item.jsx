f668de010fd5e94162f42b5c8c49bce83ae61f5d
Charlie Kim <dh77dh77@gmail.com>
2021-06-08 21:46:12 +0900

    youtube API 요소 snippet channel title 찍어보기

--- src/video_item/video_item.jsx
+++ src/video_item/video_item.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
 const VideoItem = props => (
+    <>
     <h1>
         {props.video.snippet.title}
     </h1>
+    <h3>
+        {props.video.snippet.channelTitle}
+    </h3>
+    </>
 );
 
 export default VideoItem;
