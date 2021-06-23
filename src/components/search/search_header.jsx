91cb60c55980f14695226c6c212fbac7b0d861c0
Charlie Kim <dh77dh77@gmail.com>
2021-06-23 23:55:08 +0900

    검색 기능 UI 구현

--- src/components/search/search_header.jsx
+++ src/components/search/search_header.jsx
@@ -1,15 +1,37 @@
-import React, { Component } from 'react';
+import React, { Componen, useRef } from 'react';
+
 import styles from './search_header.module.css';
 
 const SearchHeader = (props) => {
+    const inputRef = useRef();
+
+    const handleSearch = () => {
+
+    };
+
+    const onClick = () => {
+        handleSearch();
+    };
+
+    const onKeyPress = () => {
+        if(Event.onKeyPress === 'Enter') {
+            handleSearch();
+        }
+    };
+
     return (
         <header className={styles.header}>
             <div className={styles.logo}>
                 <img className={styles.img} src="/images/logo.png" alt="logo" />
                 <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input className={styles.input} type="search" placeholder="search.." />
-            <button className={styles.button} type="submit">
+            <input
+                ref={inputRef}
+                className={styles.input}
+                type="search"
+                placeholder="search.."
+                onKeyPress={onKeyPress} />
+            <button className={styles.button} type="submit" onClick={onClick}>
                 <img className={styles.buttonImg} src="/images/search.png" alt="search" />
             </button>
         </header>
