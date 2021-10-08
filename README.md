8545d3291084ff3e1a951408d8f1df2e76b56546
Charlie Kim <dh77dh77@gmail.com>
2021-10-08 09:19:39 +0900

    ReadMe 수정

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
  유튜브 API를 연동하여 키워드 검색 및 비디오 재생 기능이 있는 페이지 개발
 
  * React Hook
- * PostCSS
+ * PostCSS 모듈화
  * Restful APIs
  * Youtube APIs
  * Postman
--- src/components/service/youtube.js
+++ src/components/service/youtube.js
@@ -10,7 +10,11 @@ class youtube {
     mostPopular() {
         console.log(`####### this.getRequestOptions ${JSON.stringify(this.getRequestOptions)}`);
         
-        const response = await fetch
+        const response = await fetch(
+            `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
+            this.getRequestOptions
+        );
+        const result = await response.json();
 
 
     }
