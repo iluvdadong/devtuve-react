59a2e297a4a07bc6cafbc67551a03aad58be8d67
Charlie Kim <dh77dh77@gmail.com>
2021-10-10 22:37:05 +0900

    메인 페이지 api 호출시 개발관련 컨텐츠로 수정

--- src/app.jsx
+++ src/app.jsx
@@ -2,9 +2,15 @@ import { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import SearchHeader from './components/search/search_header';
 import VideoList from './components/video_list/video_list';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = video => {
+    setSelectedVideo(video);
+  }
 
   // Youtube class의 instance인 youtube 내부 API 함수 호출
   const search = query => {
@@ -18,14 +24,25 @@ function App({ youtube }) {
     youtube
       .mostPopular()
       .then(videos => setVideos(videos));
-
   }, []);
 
   return (
     <>
       <div className={styles.app}>
         <SearchHeader onSearch={search} />
-        <VideoList videos={videos} />
+        <section className={styles.content}>
+          {selectedVideo && (
+            <div className={styles.detail}>
+               <VideoDetail video={selectedVideo} />
+              </div>
+           )} 
+          <div className={styles.list}>
+            <VideoList 
+                videos={videos}
+                onSelectVideo={selectVideo}
+                display={selectedVideo ? 'list' : 'grid'} />
+          </div>
+        </section>
       </div>
     </>
   );
